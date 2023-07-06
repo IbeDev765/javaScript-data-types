1771fa0cd4e968560c082f5c68569cc5cb74e1a8
ibeDev768 <ibrahimalifrajab@gmail.com>
2023-07-06 18:06:32 +0800

    done bang

--- data.types.js
+++ data.types.js
@@ -2,14 +2,47 @@
 
 
 
-console.log(stringData);
-console.log(dataNumber);
-console.log(dataBoolean);
-console.log(dataUndefined);
-console.log(dataNull);
-console.log(dataObject);
-console.log(dataArray);
-console.log(dataFungsi);
-console.log(DataTime);
+//console.log(stringData);
+//var greeting = "Hello,world"
+//console.log (greeting);
+
+//console.log(dataNumber);
+//var num1 =500;
+//var num2 =3.5;
+//console.log (num1);
+//console.log (num2);
+
+//console.log(dataBoolean);
+//var istrue = true;
+//var isfalse = false;
+//console.log (istrue);
+//console.log (isfalse);
+//console.log(dataUndefined);
+//var undefinedVariable;
+ //var nullVariable = null;
+ //console.log(undefinedVariable);
+ //console.log(nullVariable);
+//console.log(dataNull);
+//var person = {
+ //   name : "baim",
+   // age : 15   
+//};
+//console.log (person);
+
+//console.log(dataObject);
+//var numbers =[1,2,3,4,5,6];
+//console.log (numbers);
+
+//console.log(dataArray);
+
+//console.log(dataFungsi);
+//function add (a,b){
+    //return a + b;
+//}
+//console.log (add (2, 3));
+//console.log(DataTime);
+
+var currentdate = new Date();
+console.log (currentdate);
 
 
